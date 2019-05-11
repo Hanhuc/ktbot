f4cddf4f9a1ad9a873dbe7abea0baac622873a92
Hanhuc <pch105@naver.com>
2019-05-11 12:29:27 +0900

    Dividing canceled

--- Camille.js
+++ Camille.js
@@ -708,6 +708,39 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
    return temp.toLowerCase();
   }
 
+  if(msg=="test") {
+    var json1 = new Object();
+    var json2 = new Object();
+    var jtot = new Object();
+    json1.name = "머저리";
+    json1.winrate = "30%";
+    jtot.pl1 = json1;
+    json2.name = "멍청이";
+    json2.winrate = "20%";
+    jtot.pl2 = json2;
+    var sjson = JSON.stringify(jtot);
+    FileStream.write("/sdcard/katalkbot/Results.json",sjson);
+    replier.reply("됐냐??");
+  }
+  if(msg=="test2") {
+    newResults(sender,hanhuc);
+  }
+  if(msg=="test3") {
+    var json = JSON.parse(FileStream.read("/sdcard/katalkbot/Results.json"));
+    var pl1 = json.pl1.name;
+    var pl2 = json.pl2.name;
+    var pl3 = json.hanhuc.name;
+    replier.reply(pl1 +" "+pl2+" "+pl3);
+  }
+  function newResults(realname,ID){
+    var json = new Object();
+    var jtot = JSON.parse(FileStream.read("/sdcard/katalkbot/Results.json"));
+    json.name = realname;
+    jtot.ID = json;
+    var sjson = JSON.stringify(jtot);
+    FileStream.write("/sdcard/katalkbot/Results.json",sjson);
+  }
+  function addResults(ID){}
 
   function battle(pl1, pl2) {
     var left = new Player(pl1);
